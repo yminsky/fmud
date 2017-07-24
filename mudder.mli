f2cf173d796ec7059c781c5272e65b387a3cf62d
Yaron Minsky <yminsky@janestreet.com>
2017-07-23 21:56:41 -0400

    Update comments.

--- mudder.mli
+++ mudder.mli
@@ -1,7 +1,18 @@
+(** A purely functional MUD engine, designed for teaching 
+
+    The engine is designed to allow the specific MUD to be written 
+    entirely using pure functions. The MUD designer decides on a
+    "world" type that contains the full state of the mud, and provides
+    an initial world and functions for handling events. Each handler
+    returns a new world and a list of actions.
+*)
 open! Core
 
+(** The actions that can be taken by the MUD engine. *)
 type action =
+  (** Send a message to a particular player, by nick *)
   | Send_message of { nick: string; message: string }
+  (** Kick a client off of the system, closing the connection *)
   | Kill_client  of { nick: string }
 [@@deriving sexp]
 
@@ -14,5 +25,5 @@ type 'world handlers =
   }
 [@@deriving sexp]
 
-(** Start the MUD server on the port in question. Never returns *)
+(** Start the MUD server on the port in question. Never returns. *)
 val start_mud : 'a handlers -> unit
